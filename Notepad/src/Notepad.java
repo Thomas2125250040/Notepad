33d95bf59c5095540be887870d442076d4daafef
Thomas2125250040 <thomassetiawan13@mhs.mdp.ac.id>
2023-11-19 17:07:59 +0700

    Create JMenuBar

--- Notepad/src/Notepad.java
+++ Notepad/src/Notepad.java
@@ -2,6 +2,9 @@
 import javax.swing.BorderFactory;
 import javax.swing.JTextArea;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JScrollPane;
 
 /*
@@ -14,21 +17,30 @@ import javax.swing.JScrollPane;
  * @author Programming
  */
 public class Notepad {
+    
     JFrame frame;
     JTextArea textArea;
     JScrollPane scrollPane;
+    JMenuBar menuBar;
+    JMenu menuFile, menuEdit, menuFormat, menuColor;
+    JMenuItem iNew ,iOpen, iSave, iSaveAs, iExit;
     
     public static void main(String[] args) {
+        
         new Notepad();
     }
     
     public Notepad(){
+        
         createWindow();
         createTextArea();
+        createMenu();
+        createMenuFile();
         frame.setVisible(true);
     }
     
     public void createWindow(){
+        
         frame = new JFrame("Notepad");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 600);
@@ -36,9 +48,46 @@ public class Notepad {
     }
     
     public void createTextArea(){
+        
         textArea = new JTextArea();
         scrollPane = new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         scrollPane.setBorder(BorderFactory.createEmptyBorder());
         frame.add(scrollPane);
+    }
+    
+    public void createMenu(){
+        
+        menuBar = new JMenuBar();
+        frame.setJMenuBar(menuBar);
+        
+        menuFile = new JMenu("File");
+        menuBar.add(menuFile);
+        
+        menuEdit = new JMenu("Edit");
+        menuBar.add(menuEdit);
+        
+        menuFormat = new JMenu("Format");
+        menuBar.add(menuFormat);
+        
+        menuColor = new JMenu("Color");
+        menuBar.add(menuColor);
+    }
+    
+    public void createMenuFile(){
+   
+        iNew = new JMenuItem("New");
+        menuFile.add(iNew);
+        
+        iOpen = new JMenuItem("Open");
+        menuFile.add(iOpen);
+        
+        iSave = new JMenuItem("Save");
+        menuFile.add(iSave);
+        
+        iSaveAs = new JMenuItem("Save As");
+        menuFile.add(iSaveAs);
+        
+        iExit = new JMenuItem("Exit");
+        menuFile.add(iExit);
     }
 }
