c16e63727b9f70e438b41d2eeebde25153d68bcc
Thomas2125250040 <thomassetiawan13@mhs.mdp.ac.id>
2023-11-19 18:41:26 +0700

    Open file function added

--- Notepad/src/FileFunction.java
+++ Notepad/src/FileFunction.java
@@ -1,5 +1,7 @@
 
 import java.awt.FileDialog;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -11,7 +13,10 @@ import java.awt.FileDialog;
  * @author Programming
  */
 public class FileFunction {
+    
     Notepad notepad;
+    String fileName;
+    String fileAddress;
     
     public FileFunction(Notepad notepad){
         
@@ -19,12 +24,35 @@ public class FileFunction {
     }
     
     public void newFile(){
+        
         notepad.textArea.setText("");
         notepad.frame.setTitle("New");
     }
     
     public void open(){
+        
         FileDialog fd = new FileDialog(notepad.frame, "Open", FileDialog.LOAD);
         fd.setVisible(true);
+        if (fd.getFile() != null){
+            
+            fileName = fd.getFile();
+            fileAddress = fd.getDirectory();
+            try {
+                
+                BufferedReader br = new BufferedReader(new FileReader(fileAddress + fileName));
+                String line = null;
+                notepad.textArea.setText("");
+                while((line = br.readLine()) != null){
+                    
+                    notepad.textArea.append(line + "\n");
+                }
+                br.close();
+            } catch (Exception e){
+                
+                System.out.println("FILE CANNNOT BE OPENED!");
+            }
+        }
+       
+     
     }
 }
