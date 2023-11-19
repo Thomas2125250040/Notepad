e9569ee41fedcb5f30989926ff0529f5114f52c4
Thomas2125250040 <thomassetiawan13@mhs.mdp.ac.id>
2023-11-19 16:43:10 +0700

    JTextArea border changed

--- Notepad/src/Notepad.java
+++ Notepad/src/Notepad.java
@@ -1,4 +1,5 @@
 
+import javax.swing.BorderFactory;
 import javax.swing.JTextArea;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
@@ -26,13 +27,14 @@ public class Notepad {
     public void createWindow(){
         frame = new JFrame("Notepad");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300, 400);
+        frame.setSize(800, 600);
         frame.setVisible(true);
     }
     
     public void createTextArea(){
         textArea = new JTextArea();
-        scrollPane = new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollPane = new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setBorder(BorderFactory.createEmptyBorder());
         frame.add(scrollPane);
     }
 }
